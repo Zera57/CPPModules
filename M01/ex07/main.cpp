664e1f79ec553d43600f59e0193f4169371c031b
Zera57 <svetik.avtonomov@gmail.com>
2021-06-10 17:05:09 +0300

    fixed M01/ex07

--- M01/ex07/main.cpp
+++ M01/ex07/main.cpp
@@ -10,31 +10,38 @@ bool replace(std::string& str, const std::string& from, const std::string& to) {
 }
 
 int main(int argc, char **argv) {
-    if (argc < 4) {
+    if (argc != 4 || ((std::string)argv[2]).length() == 0) {
         std::cout << "Invalid input" << std::endl;
-        std::cout << "\tchange <old_word> <new_word> <file_list>";
+        std::cout << "<old_word> <new_word> <file_list>" << std::endl;
+        return -1;
     }
 
     std::fstream fs;
+    fs.open(argv[1] , std::ios::in);
+    if (fs.fail()) {
+        std::cout << "Fail reading file" << std::endl;
+        fs.close();
+        return -1;
+    }
     std::fstream new_file;
-    std::string str = argv[3];
-    str += ".replace";
-    new_file.open(str, std::ios::out);  
-
+    new_file.open((std::string)argv[1] + ".replace", std::ios::out);  
+    if (new_file.fail()) {
+        std::cout << "Fail creating" << (std::string)argv[1] << "file" << std::endl;
+        fs.close();
+        new_file.close();
+        return -1;
+    }
     std::string tmp;
-    std::string oldw = argv[1];
-    std::string neww = argv[2];
+    std::string oldw = argv[2];
+    std::string neww = argv[3];
 
-    for (int i = 3; i < argc; i++) {
-        fs.open(argv[i] , std::ios::in);
-
-        while (!fs.eof()) {
-            getline(fs, tmp);
-            replace(tmp, oldw, neww);
-            new_file << tmp << std::endl;
-        }
+    while (!fs.eof()) {
+        getline(fs, tmp);
+        replace(tmp, oldw, neww);
+        new_file << tmp << std::endl;
     }
 
     fs.close();
+    new_file.close();
     return 0;   
 }
